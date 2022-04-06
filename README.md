f0bddc65fc5fede256b5ac3a2d5a3ceb3f0da368
Juan Carlos <juanc.ruiz@uam.es>
2022-04-06 19:45:50 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
-# GeneticAlgorithm
-Feature-Selector Genetic Algorithm created to choose the best subset of features from a original dataset.
+<a href="http://atvs.ii.uam.es/atvs/">
+    <img src="./media/bida.png" alt="BiDA Lab" title="Bida Lab" align="right" height="150" width="350" target="_blank"/>
+</a>
+
+# Feature-Selector Genetic Algorithm
+
+## Table of content
+
+- [Introduction](#intro)
+- [Installation](#install)
+- [Uninstallation](#uninstall)
+- [Requirements](#requirements)
+- [Genetic Algorithm](#algorithm)
+  - [Population](#population)
+  - [Evaluation](#evaluation)
+  - [Selection](#selection)
+  - [Crossover](#crossover)
+  - [Mutation](#mutation)
+- [Example of use](#example)
+
+## <a name="intro">Introduction</a>
+
+This repository contains the Python implementation of a genetic algorithm
+developed by the BiDA Lab team. This algorithm was created to choose the best
+subset of features from a original dataset.
+
+[![Feature-Selector Genetic Algorithm](./media/image.jpg)]
+
+## <a name="install">Installation</a>
+
+Simply clone this repository and run `pip setup.py install`.
+
+## <a name="uninstall">Uninstallation</a>
+
+```bash
+pip uninstall genetic_selector
+```
+
+## <a name="requirements">Requirements</a>
+
+- Python >= 3.6
+- numpy >= 1.18
+- pandas >= 1.0.1
+
+## <a name="algorithm">Genetic Algorithm</a>
+
+It is a metaheuristic algorithm based on Charles Darwin's theory of evolution.
+In particular, it is mainly inspired on the natural selection process of
+evolution, where over generations and through the use of operators such as
+mutation, crossover and selection, a positive evolution towards better
+solutions occurs.
+
+In this case, the algorithm is designed to find the subset of features that
+works best for a given problem.
+
+### <a name="population">Population</a>
+
+Each of the chromosomes belonging to the population is formed by a binary Numpy
+array, where each position refers to each of the features of the data set.
+The value one will indicate that the characteristic will be used and zero that
+it will not.
+
+### <a name="selection">Selection</a>
+
+To carry out the selection process, the entire population is evaluated using the model's average, the best chromosome is selected and the remaining chromosomes are selected per *tournament_k* size tournament.
+
+### <a name="crossover">Crossover</a>
+
+To make the crossing, mathematical hope is used. That is, if the probability of crossing is 0.5 and we have a population size of 300 (150 pairs), the mathematical hope tells us that approximately 75 crosses will be made. The crossing is done by cutting through a random position of all the genes.
+
+```bash
+n_crosses = crossover_rate * (population_size / 2)
+```
+
+### <a name="mutation">Mutation</a>
+
+For the mutation process, we again use mathematical hope. That is, if we have a mutation probability at the gene level of 0.2 and a total number of genes of 6000, then the number of mutations that will be made on average will be 1200. Mutations will be made by generating two random numbers per mutation, one to choose the chromosome and one to choose the gene to change.
+
+### <a name="evaluation">Evaluation</a>
+
+To "see how good" each of the population's chromosomes are, the average obtained from the use of a Scikit-learn classification model will be used. In addition, this model will be trained using cross-validation for validation.
+
+```bash
+n_mutations = mutation_rate * population_size * genes_per_chromosome
+```
+
+## <a name="example">Example of Use</a>
+
+```python
+import pandas as pd
+from genetic_selector import GeneticSelector
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    # You should not set the number of cores (n_jobs) in the Scikit-learn
+    # model to avoid UserWarning. The genetic selector is already parallelizable.
+    rf_clf = RandomForestClassifier(n_estimators=300)
+
+    # Load example dataset from Scikit-learn
+    dataset = datasets.load_wine()
+    X = pd.DataFrame(data=dataset['data'])
+    y = pd.Series(data=dataset['target'])
+
+    # Split into train and test
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.25)
+
+    # Create GeneticSelector instance
+    genetic_selector = GeneticSelector(rf_clf, cv=5, n_gen=50, n_population=150,
+                                        crossover_rate=0.8, mutation_rate=0.15,
+                                        tournament_k=2, return_train_score=True, initial_best_chromosome=None,
+                                        n_jobs=-1, random_state=42, verbose=0)
+    # Fit features
+    genetic_selector.fit(train_X, train_y)
+
+    # Get result
+    support = genetic_selector._support()
+    best_chromosome = support[0][0]
+    score = support[0][1]
+    best_epoch = support[0][2]
+    print(f'Best chromosome: {best_chromosome}')
+    print(f'Average score: {score}')
+    print(f'Best epoch: {best_epoch}')
+
+    test_scores = support[1]
+    train_scores = support[2]
+    chromosomes_history = support[3]
+    print(f'Test scores: {test_scores}')
+    print(f'Train scores: {train_scores}')
+    print(f'Chromosomes history: {chromosomes_history}')
+```
+
+## Bibliography
+
+- G. Chandrashekar, F. Sahin, A survey on feature selection methods, Comput. Electr. Eng. 40 (2014) 16–28. [https://doi.org/https://doi.org/10.1016/j.compeleceng.2013.11.024](https://doi.org/https://doi.org/10.1016/j.compeleceng.2013.11.024).
+
+- W.F. Punch, E.D. Goodman, M. Pei, L. Chia-Shun, P.D. Hovland, R.J. Enbody, Further Research on Feature Selection and Classification Using Genetic Algorithms, in: Proc. 5th Int. Conf. Genet. Algorithms, Morgan Kaufmann Publishers Inc., San Francisco, CA, USA, 1993: pp. 557–564.
+
+- R. Tolosana, J.C. Ruiz-Garcia, R. Vera-Rodriguez, J. Herreros-Rodriguez, S. Romero-Tapiador, A. Morales and J. Fierrez, "[Child-Computer Interaction: Recent Works, New Dataset, and Age Detection](https://arxiv.org/abs/2102.01405)", *arXiv preprint arXiv:2102.01405*, 2021.
---
+++ genetic_selector/genetic_selector.py
@@ -0,0 +1,458 @@
+# genetic_selector - Genetic algorithm for feature selection
+# Juan Carlos Ruiz García - September 2020
+
+
+import multiprocessing as mp
+import numbers
+import time
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import get_scorer
+from sklearn.base import is_classifier
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection._split import check_cv
+
+"""
+Genetic algorithm used to select the best features from a dataset
+"""
+
+
+class GeneticSelector:
+    """Feature selection with genetic algorithm.
+    Parameters
+    ----------
+    estimator : object
+        A supervised learning estimator with a `fit` method from
+        Scikit-learn.
+    scoring : str, callable, or None (default: None)
+        If None (default), uses 'accuracy' for sklearn classifiers
+        and 'r2' for sklearn regressors.
+        If str, uses a sklearn scoring metric string identifier, for example
+        {accuracy, f1, precision, recall, roc_auc} for classifiers,
+        {'mean_absolute_error', 'mean_squared_error'/'neg_mean_squared_error',
+        'median_absolute_error', 'r2'} for regressors.
+    cv : int, cross-validation generator or an iterable, default=None
+        Determines the cross-validation splitting strategy.
+        Possible inputs for cv are:
+
+        - None, to use the default 5-fold cross validation,
+        - int, to specify the number of folds in a `(Stratified)KFold`,
+        - :term:`CV splitter`,
+        - An iterable yielding (train, test) splits as arrays of indices.
+    n_gen : int, default: 50
+        Determines the maximum number of generations to be carried
+        out.
+    n_population : int, default: 100
+        It determines the number and size of the population. That
+        is, the number of chromosomes.
+    crossover_rate : double, default: 0.7
+        It defines the crossing probability. It must be a value
+        of 0.0 < crossover_rate <= 1.0.
+    mutation_rate : double, default: 0.1
+        It defines the mutation probability. It must be a value
+        of 0.0 < crossover_rate <= 1.0.
+    tournament_k : int, default: 2
+        It defines the size of the tournament carried out in the
+        selection process. Number of chromosomes to be fought per
+        tournament.
+    return_train_score : bool, default=False
+        Whether to include train scores.
+        Computing training scores is used to get insights on how different
+        parameter settings impact the overfitting/underfitting trade-off.
+        However computing the scores on the training set can be computationally
+        expensive and is not strictly required to select the parameters that
+        yield the best generalization performance.
+    initial_best_chromosome: np.ndarray, default=None
+        A 1 dimensional binary array with the size of number of features
+        defining the initial chromosome when the genetic algorithm begin.
+    n_jobs : int, default 1
+        Number of cores to run in parallel.
+        Defaults to 1 core. If `n_jobs=-1`, then number of jobs is set
+        to number of cores. If n_jobs is bigger than number of cores,
+        then number of jobs is set to number of cores.
+    random_state : int or RandomState instance, default=None
+        Controls the randomness of life cycle of each population.
+        Pass an int for reproducible output across multiple function calls.
+    verbose : int, default=0
+        Controls verbosity of output.
+    Examples
+    --------
+    An example of use.
+
+    >>> import pandas as pd
+    >>> from sklearn.ensemble import RandomForestClassifier
+    >>> from sklearn import datasets
+    >>> from sklearn.model_selection import train_test_split
+    >>> from genetic_selector import GeneticSelector
+    >>> rf_clf = RandomForestClassifier(n_estimators=100)
+    >>> dataset = datasets.load_wine()
+    >>> X = pd.DataFrame(data=dataset['data'])
+    >>> y = pd.Series(data=dataset['target'])
+    >>> train_X, test_X, train_y, test_y = train_test_split(X, y,
+    >>>     test_size=0.25)
+    >>> genetic_selector = GeneticSelector(rf_clf, score='accuracy', cv=5,
+    >>>     n_gen=50, n_population=150, crossover_rate=0.8, mutation_rate=0.15,
+    >>>     tournament_k=2, n_jobs=-1, verbose=0)
+    >>> genetic_selector.fit(train_X, train_y)
+    >>> best_chromosome = genetic_selector._support()
+    >>> print(f'Best chromosome: {best_chromosome[0]}')
+    >>> print(f'Average score: {best_chromosome[1]}')
+    """
+
+    def __init__(self, estimator: object, scoring: str = None, cv: int = 5,
+                 n_gen: int = 50, n_population: int = 100, crossover_rate:
+                 float = 0.7, mutation_rate: float = 0.1,
+                 tournament_k: int = 2, return_train_score: bool = False,
+                 initial_best_chromosome: np.ndarray = None, n_jobs: int = 1,
+                 random_state: int = None, verbose: int = 0):
+
+        # Sklearn estimator (classifier or regressor)
+        self.estimator = estimator
+        # Save train score
+        self.return_train_score = return_train_score
+        # Initial chromosome
+        self.initial_best_chromosome = initial_best_chromosome
+        # Scoring metric
+        if scoring is None:
+            if self.estimator._estimator_type == 'classifier':
+                scoring = 'accuracy'
+            elif self.est_._estimator_type == 'regressor':
+                scoring = 'r2'
+            else:
+                raise AttributeError('Estimator must '
+                                     'be a Classifier or Regressor.')
+        if isinstance(scoring, str):
+            self.scorer = get_scorer(scoring)
+        else:
+            self.scorer = scoring
+        # Number of folds in cross validation process
+        self.cv = cv
+        # Number of generations
+        if n_gen > 0:
+            self.n_gen = n_gen
+        else:
+            raise ValueError(
+                'The number of generations must be greater than 1.')
+        # Size of population (number of chromosomes)
+        self.n_population = n_population
+        # Crossover and mutations likelihood
+        if crossover_rate <= 0.0 or mutation_rate <= 0.0 or \
+                crossover_rate > 1.0 or mutation_rate > 1.0:
+            raise ValueError(
+                'Mutation and crossover rate must be a value in the range'
+                ' (0.0, 1.0].')
+        else:
+            self.mutation_rate = mutation_rate
+            self.crossover_rate = crossover_rate
+        # Tournament size in selection process
+        self.tournament_k = tournament_k
+        # Number of threads
+        if n_jobs < 0 and n_jobs == -1:
+            self.n_jobs = mp.cpu_count()
+        elif n_jobs > 0 and n_jobs <= mp.cpu_count():
+            self.n_jobs = n_jobs
+        else:
+            raise ValueError(
+                f'n_jobs == {n_jobs} is invalid! You have a maximum of'
+                f' {mp.cpu_count()} cores.')
+        # Best chromosome (np.ndarray, float, int) (chromosome, score, i_gen)
+        self.best_chromosome = (None, None, None)
+        # Population convergence variables
+        self.convergence = False
+        self.n_times_convergence = 0
+        self.threshold = 1e-6
+        # Random state
+        if isinstance(random_state, numbers.Integral):
+            np.random.seed(random_state)
+        # Verbose
+        if verbose < 0:
+            self.verbose = 0
+        else:
+            self.verbose = verbose
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        # Initialize output variables
+        self.train_scores = []
+        self.test_scores = []
+        self.chromosomes_history = []
+
+        # Check cross-validation is valid
+        self.cv = check_cv(self.cv, y, classifier=is_classifier(
+            self.estimator))
+
+        # Time when training begins
+        init_time = time.time()
+
+        # Save data and targets in the instance
+        self.X = X
+        self.y = y
+
+        # Initialize population
+        print(
+            f'# Creating initial population with {self.n_population}'
+            ' chromosomes...')
+        self.__initialize(X.shape[1])
+
+        # Insert initial best chromosome if is defined
+        if self.initial_best_chromosome is None:
+
+            # Evaluate initial population and update best_chromosome
+            print(f'# Evaluating initial population...')
+            population_scores = self.__evaluate(self.population)
+            best_chromosome_index = np.argmax(population_scores[:, 0])
+
+        elif type(self.initial_best_chromosome) == np.ndarray and \
+            len(self.initial_best_chromosome) == X.shape[1] and \
+            len(np.where((self.initial_best_chromosome <= 1) &
+                         (self.initial_best_chromosome >= 0))[0]):
+
+            # Introduce the best in the population
+            index_insertion = np.random.randint(self.n_population)
+            self.population[index_insertion] = self.initial_best_chromosome
+
+            # Evaluate initial population and update best_chromosome
+            print(f'# Evaluating initial population...')
+            population_scores = self.__evaluate(self.population)
+            best_chromosome_index = index_insertion
+
+        else:
+            raise ValueError('Initial best chromosome must be a 1 '
+                             'dimensional binary array with a length of '
+                             'X.shape[1]')
+
+        # Update best chromosome found
+        self.__update_best_chromosome(
+            self.population, population_scores[:, 0],
+            best_chromosome_index, 0)
+
+        # Save output results
+        if self.return_train_score:
+            self.__save_output_results(
+                test_score=population_scores[best_chromosome_index, 0],
+                best_current_chromosome=self.population[best_chromosome_index],
+                train_score=population_scores[best_chromosome_index, 1])
+        else:
+            self.__save_output_results(
+                test_score=population_scores[best_chromosome_index, 0],
+                best_current_chromosome=self.population[best_chromosome_index])
+
+        # Loop until evaluation converge
+        i = 0
+        while i < self.n_gen and not self.convergence:
+            # Time when generation begins
+            generation_time = time.time()
+
+            print(f'\n# Creating generation {i+1}...')
+            # Selection
+            new_population = self.__selection(
+                self.population, population_scores[:, 0],
+                best_chromosome_index)
+            print(f'# Selection {i+1} done.')
+
+            # Crossover
+            new_population = self.__crossover(new_population)
+            print(f'# Crossover {i+1} done.')
+
+            # Mutation
+            new_population = self.__mutation(new_population)
+            print(f'# Mutation {i+1} done.')
+
+            # Replace previous population with new_population
+            self.population = new_population.copy()
+
+            # Evaluate new population and update best_chromosome
+            print(f'# Evaluating population of new generation {i}...')
+            population_scores = self.__evaluate(self.population)
+            best_chromosome_index = np.argmax(population_scores[:, 0])
+            self.__update_best_chromosome(
+                self.population, population_scores[:, 0],
+                best_chromosome_index, i+1)
+
+            # Save output results
+            if self.return_train_score:
+                self.__save_output_results(
+                    test_score=population_scores[best_chromosome_index, 0],
+                    best_current_chromosome=self.population
+                    [best_chromosome_index],
+                    train_score=population_scores[best_chromosome_index, 1])
+            else:
+                self.__save_output_results(
+                    test_score=population_scores[best_chromosome_index, 0],
+                    best_current_chromosome=self.population
+                    [best_chromosome_index])
+
+            # Next generation
+            i = i + 1
+
+            # Time when generation ends
+            elapsed_generation_time = time.time() - generation_time
+            print(
+                '# Elapsed generation time: %.2f seconds' %
+                elapsed_generation_time)
+
+        # Time when training ends
+        elapsed_time = time.time() - init_time
+        print('# Elapsed time: %.2f seconds' % elapsed_time)
+
+    def __save_output_results(self, test_score: float,
+                              best_current_chromosome: np.ndarray,
+                              train_score: float = None):
+        """
+
+        Private function to save the output results in their respective
+        variables.
+
+        Args:
+            test_score (float): Best test score achieved in the present
+            generation for the best chromosome.
+            train_score (float): Trainig score achieved in the present
+            generation for the best chromosome.
+            best_current_chromosome (np.ndarray): That chromosome whose
+                test_score was the best one found in the present generation.
+        """
+        self.test_scores.append(test_score)
+        if train_score is not None:
+            self.train_scores.append(train_score)
+        self.chromosomes_history.append(np.where(best_current_chromosome)[0])
+
+    def _support(self) -> np.ndarray:
+        """
+            Return an array with 4 values:
+                - best_chromosome : tuple
+                    Tuple with the values (np.ndarray, float, int) =>
+                    (chromosome, score, i_gen)
+                - test_scores : np.ndarray
+                    An array with test scores during each generation.
+                - train_scores : np.ndarray
+                    An array with training scores during each generation. Could
+                    be `None` if self.return_train_score = False.
+                - chromosomes_history : np.ndarray
+                    An array with multiple mask of selected features, each one
+                    for the best chromosome found in each generation.
+        """
+
+        return np.array([
+            self.best_chromosome,
+            self.test_scores,
+            self.train_scores if self.return_train_score else None,
+            self.chromosomes_history
+        ])
+
+    def __initialize(self, n_genes: int):
+        # Create n_population chromosomes
+        self.population = np.random.randint(
+            2, size=(self.n_population, n_genes))
+
+    def estimate(self, chromosome: np.ndarray) -> float:
+        # Select those features with ones in chromosome
+        data = self.X.loc[:, chromosome.astype(bool)]
+
+        # Cross-validation execution
+        scores = cross_validate(
+            self.estimator, data, self.y, cv=self.cv, scoring=self.scorer,
+            verbose=self.verbose, return_train_score=self.return_train_score)
+
+        if self.return_train_score:
+            return (scores['test_score'].mean(),
+                    scores['train_score'].mean())
+        else:
+            return (scores['test_score'].mean(), None)
+
+    def __evaluate(self, population: np.ndarray) -> np.ndarray:
+        # Pool for parallelization
+        pool = mp.Pool(self.n_jobs)
+        return np.array(pool.map(self.estimate, population))
+
+    def __selection(
+            self, population: np.ndarray, population_scores: np.ndarray,
+            best_chromosome_index: int) -> np.ndarray:
+        # Create new population
+        new_population = [population[best_chromosome_index]]
+
+        # Tournament_k chromosome tournament until fill the numpy array
+        while len(new_population) != self.n_population:
+            # Generate tournament_k positions randomly
+            k_chromosomes = np.random.randint(
+                len(population), size=self.tournament_k)
+            # Get the best one of these tournament_k chromosomes
+            best_of_tournament_index = np.argmax(
+                population_scores[k_chromosomes])
+            # Append it to the new population
+            new_population.append(
+                population[k_chromosomes[best_of_tournament_index]])
+
+        return np.array(new_population)
+
+    def __crossover(self, population: np.ndarray) -> np.ndarray:
+        # Define the number of crosses
+        n_crosses = int(self.crossover_rate * int(self.n_population / 2))
+
+        # Make a copy from current population
+        crossover_population = population.copy()
+
+        # Make n_crosses crosses
+        for i in range(0, n_crosses*2, 2):
+            cut_index = np.random.randint(1, self.X.shape[1])
+            tmp = crossover_population[i, cut_index:].copy()
+            crossover_population[i, cut_index:], crossover_population[i+1,
+                                                                      cut_index:] = crossover_population[i+1, cut_index:], tmp
+            # Avoid null chromosomes
+            if not all(crossover_population[i]):
+                crossover_population[i] = population[i]
+            if not all(crossover_population[i+1]):
+                crossover_population[i+1] = population[i+1]
+
+        return crossover_population
+
+    def __mutation(self, population: np.ndarray) -> np.ndarray:
+        # Define number of mutations to do
+        n_mutations = int(
+            self.mutation_rate * self.n_population * self.X.shape[1])
+
+        # Mutating n_mutations genes
+        for _ in range(n_mutations):
+            chromosome_index = np.random.randint(0, self.n_population)
+            gene_index = np.random.randint(0, self.X.shape[1])
+            population[chromosome_index, gene_index] = 0 if \
+                population[chromosome_index, gene_index] == 1 else 1
+
+        return population
+
+    def __update_best_chromosome(
+            self, population: np.ndarray, population_scores: np.ndarray,
+            best_chromosome_index: int, i_gen: int):
+        # Initialize best_chromosome
+        if self.best_chromosome[0] is None and self.best_chromosome[1] is None:
+            self.best_chromosome = (
+                population[best_chromosome_index],
+                population_scores[best_chromosome_index],
+                i_gen)
+            self.threshold_times_convergence = 5
+        # Update if new is better
+        elif population_scores[best_chromosome_index] > \
+                self.best_chromosome[1]:
+            if i_gen >= 17:
+                self.threshold_times_convergence = int(np.ceil(0.3 * i_gen))
+            self.best_chromosome = (
+                population[best_chromosome_index],
+                population_scores[best_chromosome_index],
+                i_gen)
+            self.n_times_convergence = 0
+            print(
+                '# (BETTER) A better chromosome than the current one has '
+                f'been found ({self.best_chromosome[1]}).')
+        # If is smaller than self.threshold count it until convergence
+        elif abs(population_scores[best_chromosome_index] -
+                 self.best_chromosome[1]) <= self.threshold:
+            self.n_times_convergence = self.n_times_convergence + 1
+            print(
+                f'# Best average value found {self.n_times_convergence}'
+                f'/{self.threshold_times_convergence} times.')
+            if self.n_times_convergence == self.threshold_times_convergence:
+                self.convergence = True
+        else:
+            self.n_times_convergence = 0
+            print('# (WORST) No better chromosome than the current one has '
+                  f'been found ({population_scores[best_chromosome_index]}).')
+        print(f'# Current best chromosome: {self.best_chromosome}')
